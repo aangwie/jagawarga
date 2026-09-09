fe5e9100505bf3615b122529128ebfecf58efa2d
aangwie <wirawan.aang5@gmail.com>
2026-09-09 19:16:56 +0700

    Hide Login On Navbar when view on Mobile Mode

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -235,7 +235,7 @@
 
                 <!-- Autentikasi Pengguna: Login / Akun Aktif & Logout -->
                 @guest
-                    <a href="{{ route('login') }}" class="flex items-center gap-1.5 px-3 py-1.5 rounded-xl bg-slate-100 hover:bg-emerald-50 text-slate-700 hover:text-emerald-700 border border-slate-200/80 text-xs font-bold transition cursor-pointer">
+                    <a href="{{ route('login') }}" class="hidden md:flex items-center gap-1.5 px-3 py-1.5 rounded-xl bg-slate-100 hover:bg-emerald-50 text-slate-700 hover:text-emerald-700 border border-slate-200/80 text-xs font-bold transition cursor-pointer">
                         <svg class="w-4 h-4 text-emerald-600" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M11 16l-4-4m0 0l4-4m-4 4h14m-5 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h7a3 3 0 013 3v1" />
                         </svg>
